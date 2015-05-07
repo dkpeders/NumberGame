18ffb96f31882e77dc9d781fc32744cc664226e1
dkpeders <dkpeders@umich.edu>
2015-05-06 21:36:31 -0400

    Final Answer

--- NumberGame/NumberGame/NumberGame.cpp
+++ NumberGame/NumberGame/NumberGame.cpp
@@ -5,42 +5,45 @@ using namespace std;
 
 int main()
 {
-	cout << "Guess a number between 1 and 100!" << endl;
-	srand(time(0));
-	int randNum = rand() % 100 + 1;
-	int x, i;
-
-	for (i = 1; i < 11; i++)
+	while (true)
 	{
-		cout << "Guess #" << i << ": ";
-		cin >> x;
-		if (x < randNum)
+		cout << "Guess a number between 1 and 100!" << endl;
+		srand(time(0));
+		int randNum = rand() % 100 + 1;
+		int x, i;
+
+		for (i = 1; i < 11; i++)
 		{
-			cout << "Too Low!";
+			cout << "Guess #" << i << ": ";
+			cin >> x;
+			if (x < randNum)
+			{
+				cout << "Too Low!";
+			}
+			else if (x > randNum)
+			{
+				cout << "Too High!";
+			}
+			else
+			{
+				break;
+			}
 		}
-		else if (x > randNum)
+		if (i == 11)
 		{
-			cout << "Too High!";
+			cout << "You Lose!";
 		}
 		else
 		{
+			cout << "You Win!";
+		}
+
+		char n;
+		cout << "Would you like to play again (Y/N)? ";
+		cin >> n;
+		if (n == 'N' || n == 'n')
+		{
 			break;
 		}
-	}
-	if (i == 11)
-	{
-		cout << "You Lose!";
-	}
-	else
-	{
-		cout << "You Win!";
-	}
-	char n;
-	cout << "Would you like to play again?";
-	cin >> n;
-	while (true)
-	{
-		if (n);
-		break;
 	}
 }
